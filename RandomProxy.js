7254b7b8d811ca77350dd1f9e577161b07bf1d9d
Max Base <MaxBaseCode@Gmail.Com>
2018-06-26 11:01:13 +0430

    Update RandomProxy.js

--- RandomProxy.js
+++ RandomProxy.js
@@ -12,30 +12,45 @@
 {
 	"use strict";
 	/**
+	* @variable link
+	*
+	* @goal : link of free API
+	*
+	* @return string
+	**/
+	//var link="https://api.ronite.com/proxy/";
+	var link="http://api.ronite.com/proxy/";
+	/**
 	* @function get
 	*
 	* @goal : get a proxy from API
 	*
-	* @return struct
+	* @return void
 	**/
-	var get=function()
+	var get=function(callback)
 	{
 		var http;
 		if(window.XMLHttpRequest)
+		{
+			//XMLHttpRequest(Chrome, Firefox, IE7+, Edge, Safari Opera)
 			http=new XMLHttpRequest();
+		}
 		else
+		{
+			//ActiveXObject(IE5 and IE6)
 			http=new ActiveXObject("Microsoft.XMLHTTP");
+		}
 		http.onreadystatechange=function()
 		{
 			if(this.readyState == 4 && this.status == 200)
 			{
 				var objs=JSON.parse( this.responseText );
-				return objs;
+				callback(objs);
 			}
 			//What will happen when a problem occurs?
 			//such as : 'down server' , 'low speed' or more...
 		};
-		http.open("GET","http://api.ronite.com/proxy/",true);
+		http.open("GET",link,true);
 		http.send();
 	};
 	/**
